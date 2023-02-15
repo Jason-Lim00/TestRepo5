f8f5876f5d855d36a3db0d17462dbe3f130f87f9
Jason-Lim00 <95932831+Jason-Lim00@users.noreply.github.com>
2023-02-15 12:18:08 -0600

    Update Test-change-pw.py

--- Test-change-pw.py
+++ Test-change-pw.py
@@ -1,8 +1,12 @@
 def forgot_password(request, reset_key):
     test = 2
     print("stuff")
-
+    
+    test = 3
     
 def reset_password(request, reset_key):
     test = 1
+    print("stuff")
+
+
     print("stuff")
