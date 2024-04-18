33d658d6b7a7d134080dd965484a348a712adf3d
Jason Lim <95932831+Jason-Lim00@users.noreply.github.com>
2024-04-18 12:18:04 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,70 @@
-apply plugin: 'java'
-apply plugin: 'eclipse'
-apply plugin: 'application'
+// apply plugin: 'java'
+// apply plugin: 'eclipse'
+// apply plugin: 'application'
 
-mainClassName = 'hello.HelloWorld'
+// mainClassName = 'hello.HelloWorld'
 
-// tag::repositories[]
-repositories { 
-    mavenCentral() 
+// // tag::repositories[]
+// repositories { 
+//     mavenCentral() 
+// }
+// // end::repositories[]
+
+// dependencyLocking {
+//   lockAllConfigurations()
+// }
+
+// // tag::jar[]
+// jar {
+//     archiveBaseName = 'gs-gradle'
+//     archiveVersion =  '0.1.0'
+// }
+// // end::jar[]
+
+// // tag::dependencies[]
+// sourceCompatibility = 1.8
+// targetCompatibility = 1.8
+
+// dependencies {
+//     implementation "joda-time:joda-time:2.2"
+//     testImplementation "junit:junit:4.12"
+//     implementation 'org.springframework.boot:spring-boot-starter:2.3.4.RELEASE'
+//     testImplementation 'org.springframework.boot:spring-boot-starter-test:2.3.4.RELEASE'
+// }
+// // end::dependencies[]
+
+// // tag::wrapper[]
+// // end::wrapper[]
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.1.3")
+        classpath(kotlin("gradle-plugin", version = "1.3.70"))
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
 }
-// end::repositories[]
 
-dependencyLocking {
-  lockAllConfigurations()
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
 }
 
-// tag::jar[]
-jar {
-    archiveBaseName = 'gs-gradle'
-    archiveVersion =  '0.1.0'
+configurations {
+    compileClasspath {
+        resolutionStrategy.activateDependencyLocking()
+    }
 }
-// end::jar[]
 
-// tag::dependencies[]
-sourceCompatibility = 1.8
-targetCompatibility = 1.8
-
-dependencies {
-    implementation "joda-time:joda-time:2.2"
-    testImplementation "junit:junit:4.12"
-    implementation 'org.springframework.boot:spring-boot-starter:2.3.4.RELEASE'
-    testImplementation 'org.springframework.boot:spring-boot-starter-test:2.3.4.RELEASE'
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
-// end::dependencies[]
-
-// tag::wrapper[]
-// end::wrapper[]
